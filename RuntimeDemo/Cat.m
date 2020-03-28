f49fe4e2014a477db7f9ceac80bbad948c289ef7
chinwy <amberchin@163.com>
2020-03-28 14:56:18 +0800

    no message

--- RuntimeDemo/Cat.m
+++ RuntimeDemo/Cat.m
@@ -17,33 +17,23 @@
     NSLog(@"catSleep");
 }
 
-//1.动态方法解析 添加一个方法实现
-//+ (BOOL)resolveInstanceMethod:(SEL)sel {
-//    if (sel == NSSelectorFromString(@"run")){
-//        class_addMethod(objc_getClass("Cat"), sel, [[Dog new] methodForSelector:@selector(eat)], "v@:");
-//        return YES;
-//    }
-//    return [super resolveInstanceMethod:sel];
-//}
 
-+ (BOOL)resolveClassMethod:(SEL)sel
-{
-    if (sel == @selector(sleep)) {
-        Method method = class_getClassMethod(self, @selector(catSleep));
+//1.动态方法解析 添加一个类方法实现
++ (BOOL)resolveClassMethod:(SEL)sel{
+    if (sel == NSSelectorFromString(@"sleep")){
+        Method method = class_getClassMethod([[Dog new] class], @selector(sleep));
         class_addMethod(object_getClass(self), sel, method_getImplementation(method), method_getTypeEncoding(method));
-        return YES;
     }
     return [super resolveClassMethod:sel];
 }
 
-
 //2. 如果上一步返回NO 进入消息转发流程 让其他类去相应消息
-//- (id)forwardingTargetForSelector:(SEL)aSelector{
-//    if (aSelector == NSSelectorFromString(@"run")){
-//        return [Dog new];
-//    }
-//    return [super forwardingTargetForSelector:aSelector];
-//}
+- (id)forwardingTargetForSelector:(SEL)aSelector{
+    if (aSelector == NSSelectorFromString(@"run")){
+        return [Dog new];
+    }
+    return [super forwardingTargetForSelector:aSelector];
+}
 
 //3.如果上一步没有实现 就会进入方法签名流程
 - (NSMethodSignature *)methodSignatureForSelector:(SEL)aSelector{
@@ -54,10 +44,9 @@
 }
 
 - (void)forwardInvocation:(NSInvocation *)anInvocation{
-    if (anInvocation.selector == NSSelectorFromString(@"run")){
+    if (anInvocation.selector ==  NSSelectorFromString(@"run")){
         return [anInvocation invokeWithTarget:[Dog new]];
     }
     return [super forwardInvocation:anInvocation];
 }
-
 @end
